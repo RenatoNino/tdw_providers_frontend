bc06ffa9f238c38e96a32456e45b737ff1541f7d
RenatoNino <jorgenino07@gmail.com>
2023-07-10 14:28:52 -0500

    feat(modals): Create modal implemented

--- src/components/modals/ProviderDetailModal.js
+++ src/components/modals/ProviderDetailModal.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button, Modal, Form } from "react-bootstrap";
+import { Button, Modal, Form, Row, Col } from "react-bootstrap";
+import proiverImageDefault from "../../assets/images/provider_default.jpg";
 
 // eslint-disable-next-line react/prop-types
 const ProviderDetailModal = ({ show, provider, handleClose }) => {
@@ -8,7 +9,37 @@ const ProviderDetailModal = ({ show, provider, handleClose }) => {
       <Modal.Header closeButton>
         <Modal.Title>Información detallada del proveedor</Modal.Title>
       </Modal.Header>
-      <Modal.Body></Modal.Body>
+      <Modal.Body>
+        <Row>
+          <Col className="text-center">
+            <img src={provider.image} alt="Imagen Proveedor" width="50%"></img>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="fw-bold">RUC:</Col>
+          <Col>{provider.ruc}</Col>
+        </Row>
+        <Row>
+          <Col className="fw-bold">Razón Social:</Col>
+          <Col>{provider.business_name}</Col>
+        </Row>
+        <Row>
+          <Col className="fw-bold">Representante:</Col>
+          <Col>{provider.representative}</Col>
+        </Row>
+        <Row>
+          <Col className="fw-bold">Rubro:</Col>
+          <Col>{provider.sector}</Col>
+        </Row>
+        <Row>
+          <Col className="fw-bold">Dirección:</Col>
+          <Col>{provider.address}</Col>
+        </Row>
+        <Row>
+          <Col className="fw-bold">Correo:</Col>
+          <Col>{provider.email}</Col>
+        </Row>
+      </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
           Cerrar
